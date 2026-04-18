b0647f142357323ec0e663dbbac54a90c0482b0b
TeseySTD <skoreykomisha499@gmail.com>
2026-04-18 15:13:03 +0300

    fix(angular):Home link bug

--- angular-app/src/app/features/home/pages/home.ts
+++ angular-app/src/app/features/home/pages/home.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ArticleCard } from '@/shared/ui/article-card/article-card';
 import { ArticleStore } from '@/core/state/article.store';
 import { Loader } from '@/shared/ui/loader/loader';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ArticleCard, Loader],
+  imports: [CommonModule, ArticleCard, Loader, RouterLink],
   templateUrl: './home.html',
 })
 export class Home implements OnInit {
